026877e0bd1c09f395f0fbdb2a34654956708145
niedzwiedzw <niedzwiedzwo@gmail.com>
2025-03-20 14:53:46 +0100

    depth

--- crates/fire-and-sword/src/run/rendering.rs
+++ crates/fire-and-sword/src/run/rendering.rs
@@ -90,6 +90,7 @@ pub struct State<'a> {
     pub camera_buffer: wgpu::Buffer,
     pub instance_buffer: wgpu::Buffer,
     pub instances: Vec<Instance>,
+    pub depth_texture: texture::Texture,
 }
 
 impl<'a> State<'a> {
@@ -152,7 +153,7 @@ impl<'a> State<'a> {
             .context("requesting device and queue")?;
 
         surface.configure(&device, &config);
-
+        let depth_texture = texture::Texture::depth_texture(&device, (config.width, config.height), "depth texture");
         let diffuse_texture = texture::Texture::from_bytes(&device, &queue, include_bytes!("../../../../assets/happy-tree.png"), "happy-tree.png")
             .context("loading happy little tree")?;
         info!("loaded happy tree");
@@ -348,7 +349,13 @@ impl<'a> State<'a> {
                 unclipped_depth: false,
                 conservative: false,
             },
-            depth_stencil: None,
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: texture::Texture::DEPTH_FORMAT,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
             multisample: wgpu::MultisampleState {
                 count: 1,
                 mask: !0,
@@ -372,6 +379,7 @@ impl<'a> State<'a> {
             camera_buffer,
             instances,
             instance_buffer,
+            depth_texture,
         })
     }
 
@@ -380,6 +388,7 @@ impl<'a> State<'a> {
             self.size = new_size;
             self.config.width = new_size.width;
             self.config.height = new_size.height;
+            self.depth_texture = texture::Texture::depth_texture(&self.device, self.config.pipe_ref(|c| (c.width, c.height)), "depth texture");
             self.surface.configure(&self.device, &self.config);
         }
     }
@@ -437,7 +446,14 @@ impl<'a> State<'a> {
                                             }),
                                         },
                                     })],
-                                    depth_stencil_attachment: None,
+                                    depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                                        view: &self.depth_texture.view,
+                                        depth_ops: Some(wgpu::Operations {
+                                            load: wgpu::LoadOp::Clear(1.),
+                                            store: wgpu::StoreOp::Store,
+                                        }),
+                                        stencil_ops: None,
+                                    }),
                                     timestamp_writes: None,
                                     occlusion_query_set: None,
                                 })
